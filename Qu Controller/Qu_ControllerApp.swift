fba6cc72d7342d8a6a2a54e3e63a6d37e7cc441c
Gavin Song <gavin.song@gumtreegroup.com.au>
2026-07-02 10:27:26 +1000

    Initial Commit

---
+++ Qu Controller/Qu_ControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Qu_ControllerApp.swift
+//  Qu Controller
+//
+//  Created by Gavin Song on 2/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct Qu_ControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
